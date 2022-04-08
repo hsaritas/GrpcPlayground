c899c5713d6536850d7ddb97e7959c45a84978d5
halis.saritas <halis.saritas@dstrace.com>
2022-04-08 13:46:58 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿
+using Grpc.Core;
+using Grpc.Net.Client;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Run(() => MainAsync(args)).Wait();
+            Console.WriteLine($"Press any key :)");
+            Console.ReadKey();
+        }
+
+        static async void MainAsync(string[] args)
+        {
+            try
+            {
+                using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+                var client = new MongoSubscription.MongoSubscriptionClient(channel);
+                var reply = await client.SayHelloAsync(
+                                  new HelloRequest { Name = "GreeterClient" });
+                Console.WriteLine($"Greeting: Message:{reply.Message}, Name: {reply.Name} ");
+
+                var cts = new CancellationTokenSource();
+                using var streamingCall = client.GetMongoStream(new MongoSubscriptionRequest() { ClientId = 12345, FilterMethod= "oRtak" }, cancellationToken: cts.Token);
+
+                while (streamingCall.ResponseStream.MoveNext(cts.Token).Result)
+                {
+                    BsonDocument doc = BsonSerializer.Deserialize<BsonDocument>(streamingCall.ResponseStream.Current.Message);
+                    Console.WriteLine($"{doc["Properties"]["HttpLogModel"]["Request"]["Path"]}");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine($"{ex.Message}"); 
+            }
+
+
+        }
+    }
+}
---
+++ GrpcService1/Services/GreeterService.cs
@@ -0,0 +1,77 @@
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace GrpcService1
+{
+    public class GreeterService : MongoSubscription.MongoSubscriptionBase
+    {
+        private readonly ILogger<GreeterService> _logger;
+        public GreeterService(ILogger<GreeterService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override async Task GetMongoStream(MongoSubscriptionRequest request, IServerStreamWriter<MongoSubscriberEvent> responseStream, ServerCallContext context)
+        {
+            try
+            {
+                var client = new MongoClient("mongodb://192.168.141.111:27017");
+                var database = client.GetDatabase("DSGTS");
+                var collection = database.GetCollection<BsonDocument>("DS.GTS.API");
+
+                var builder = Builders<ChangeStreamDocument<BsonDocument>>.Filter;
+                var filter = builder.Where(x => x.OperationType == ChangeStreamOperationType.Insert);
+
+                if (string.IsNullOrEmpty(request.FilterMethod) == false)
+                {
+                    filter &= builder.Regex("FullDocument.Properties.HttpLogModel.Request.Path", new BsonRegularExpression(new Regex(request.FilterMethod, RegexOptions.IgnoreCase)));
+                }
+
+                var pipeline = new IPipelineStageDefinition[]
+                    {
+                        PipelineStageDefinitionBuilder.Match(filter)
+                    };
+                var options = new ChangeStreamOptions { FullDocument = ChangeStreamFullDocumentOption.UpdateLookup };
+
+                using var cursor = collection.Watch<ChangeStreamDocument<BsonDocument>>(pipeline, options);
+
+                var enumerator = cursor.ToEnumerable().GetEnumerator();
+                while (enumerator.MoveNext() && !context.CancellationToken.IsCancellationRequested)
+                {
+                    ChangeStreamDocument<BsonDocument> doc = enumerator.Current;
+
+                    Console.WriteLine();
+                    var mongoEvent = new MongoSubscriberEvent
+                    {
+                        DateTimeStamp = Timestamp.FromDateTime(DateTime.UtcNow),
+                        Id = request.ClientId,
+                        Message = doc.FullDocument.ToJson()
+                    };
+
+                    _logger.LogInformation("MongoEvent Sent");
+
+                    await responseStream.WriteAsync(mongoEvent);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new HelloReply
+            {
+                Message = "Hello " + request.Name,
+                Name = "My Name is " + request.Name,
+            });
+        }
+    }
+}
